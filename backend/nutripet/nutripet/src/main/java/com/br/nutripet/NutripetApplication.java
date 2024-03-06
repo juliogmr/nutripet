8c0505977f8a15e7cc0acb82feb2c3a5ae7ca957
Julio Rocha <juliogabrielmr@gmail.com>
2024-03-06 18:37:46 -0300

    add backend project

---
+++ backend/nutripet/nutripet/src/main/java/com/br/nutripet/NutripetApplication.java
@@ -0,0 +1,13 @@
+package com.br.nutripet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutripetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutripetApplication.class, args);
+	}
+
+}
---
+++ backend/nutripet/nutripet/src/test/java/com/br/nutripet/NutripetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.nutripet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutripetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
